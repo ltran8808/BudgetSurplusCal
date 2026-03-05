e14cd9a97cbad74e9b20059926eb14719156b8ac
Long Tran <blong1202@gmail.com>
2026-03-04 23:12:28 -0600

    See NotesToSelf

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.ltdev.budgetsurpluscalculator"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
